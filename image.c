cce91eae8c82368de48c1235b0f2fd1f238441ae
Alejandro Castro <alejandrocastro9b@hotmail.com>
2017-02-26 10:25:32 -0500

    Creacion archivo con main

---
+++ image.c
@@ -0,0 +1,17 @@
+/*
+  Archivo: image.c
+  Realizado por: Pablo Ariza y Alejandro Castro
+  Contiene: funcion main para ejecucion del programa.
+  Fecha última modificación: 26/02/2017
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv){
+  if(argc != 2){
+    printf("Error en ingreso de parametros.");
+    exit(-1);
+  }
+  return 0;
+}
